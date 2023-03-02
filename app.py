5d6793db80274868b0f81cd68f6f08c0ce1e257a
Crystal Xu <u6191391@anu.edu.au>
2023-03-02 23:28:26 +1100

    model with inventory data

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ conn = snowflake.connector.connect(
 def generate_sql(query):
     response = openai.Completion.create(
         model="code-davinci-002",
-        prompt="### Snowflake SQL tables, with their properties:\n#\n# EMPLOYEES(EMPLOYEE_ID, FIRST_NAME, LAST_NAME, DISPLAY_NAME)\n# HOSPITALS(HOSPITAL_ID, HOSPITAL_NAME)\n# WARDS(WARD_ID, WARD_NAME)\n# TAGS(TAG_ID, TAG_NAME)\n# SHIFT_TYPE(SHIFT_TYPE_ID, SHIFT_NAME, SHIFT_START_TIME)\n# SHIFTS(SHIFT_DATE, HOSPITAL_ID, WARD_ID, SHIFT, SHIFT_TYPE_ID, START_DT, END_DT, EMPLOYEE_ID, TAG_ID)\n#\n### A query to "+query+"\nSELECT",
+        prompt="### Snowflake SQL tables, with their properties:\n#\n# EMPLOYEES(EMPLOYEE_ID, FIRST_NAME, LAST_NAME, DISPLAY_NAME)\n# HOSPITALS(HOSPITAL_ID, HOSPITAL_NAME)\n# WARDS(WARD_ID, WARD_NAME)\n# TAGS(TAG_ID, TAG_NAME)\n# SHIFT_TYPE(SHIFT_TYPE_ID, SHIFT_NAME, SHIFT_START_TIME)\n# SHIFTS(SHIFT_DATE, HOSPITAL_ID, WARD_ID, SHIFT, SHIFT_TYPE_ID, START_DT, END_DT, EMPLOYEE_ID, TAG_ID)\n# INVENTORY_TYPE(INVENTORY_TYPE_ID, NAME, DESCRIPTION)\n# INVENTORY(INVENTORY_ID, INVENTORY_TYPE_ID, COMMISSIONED_DATE, IS_RETIRED)\n# PATIENTS(PATIENT_ID, NAME)\n# INVENTORY_ASSIGNMENTS(ASSIGNMENT_ID, INVENTORY_ID, PATIENT_ID, ASSIGNED_DATETIME, HOSPITAL_ID, WARD_ID, ASSIGNED_BY)\n#\n### A query to "+query+"\nSELECT",
         #"Generate SQL query to "+query,
         temperature=0,
         max_tokens=1024,
@@ -52,13 +52,15 @@ def get_data(sql_query):
     return data
 
 # Create a function to execute SQL query
-def execute_sql_query(sql_query):
+def execute_sql_query(query, sql_query):
     try:
         data = get_data(sql_query)
         if data.empty:
             st.error('There is no data for your query!')
         else:
-            with st.expander("See SQL query"):
+            with st.expander("See question:"):
+                st.write(query)
+            with st.expander("See SQL query:"):
                 st.write(sql_query)
             # Display the data in a table
             st.write("Here's your data:")
@@ -87,9 +89,9 @@ col2_1, col2_2, col2_3, col2_4 = st.columns([3,3,1,2.5])
 # Get user input using ChatGPT
 with col2_2:
     st.text_input(
-        label="",
+        label="Communicate witn your data",
         label_visibility="hidden",
-        placeholder="Ask a question about your data: eg. retrieve all employees",
+        placeholder="Ask a question about your data: eg. Retrieve all employees",
         value="",
         key="user_input",
     )
@@ -99,44 +101,50 @@ with col2_3:
     st.write("")
     button_search = st.button("Search")
 
+# Example questions
+question_01 = "List all shifts which started on 5th Feb 2023 for the hospital named West Port Medical"
+question_02 = "Which employee has the most evening shifts between 5th Feb 2023 and 15th Feb 2023"
+question_03 = "List all morning shifts belong to employees whose names are Andrew Krotz and Caroline Laurin"
+question_04 = "Which hospital has the most shifts between 5th Feb 2023 and 10th Feb 2023"
+
 col3_1, col3_2, col3_3 = st.columns([3,4,2.5])
 with col3_2:
     st.write("Popular Questions:")
 
-col4_1, col4_2, col4_3, col4_4, col4_5 = st.columns([3,1.8,1.8,0.9,2])
+col4_1, col4_2, col4_3, col4_4, col4_5 = st.columns([3,1.85,1.85,0.8,2])
 
 with col4_2:  
-    button_example1 = st.button("List all shifts which started on 5th Feb 2023 for the hospital named West Port Medical")
+    button_example1 = st.button(question_01)
 with col4_3:
-    button_example2 = st.button("Which employee has the most evening shifts between 5th Feb 2023 and 15th Feb 2023")
+    button_example2 = st.button(question_02)
 
-col5_1, col5_2, col5_3, col5_4, col5_5 = st.columns([3,1.8,1.8,0.9,2])
+col5_1, col5_2, col5_3, col5_4, col5_5 = st.columns([3,1.85,1.85,0.8,2])
 with col5_2:
-    button_example3 = st.button("List all morning shifts which belong to employees whose names are Andrew Krotz and Caroline Laurin")
+    button_example3 = st.button(question_03)
 with col5_3:
-    button_example4 = st.button("Which hospital has the most shifts between 5th Feb 2023 and 10th Feb 2023")
+    button_example4 = st.button(question_04)
 
 # Retrieve data from Snowflake using the SQL query
 col6_1, col6_2, col6_3 = st.columns([1,3,1])
 with col6_2:
     if button_search:
         if st.session_state.user_input == "":
-            sql_query = generate_sql("retrieve all employees")
-            execute_sql_query(sql_query)
+            sql_query = generate_sql("Retrieve all employees")
+            execute_sql_query("Retrieve all employees", sql_query)
         else:
             sql_query = generate_sql(st.session_state.user_input)
-            execute_sql_query(sql_query)
+            execute_sql_query(st.session_state.user_input, sql_query)
     elif button_example1:
-        sql_query = generate_sql("List all shifts which started on 5th Feb 2023 for the hospital named West Port Medical")
-        execute_sql_query(sql_query)
+        sql_query = generate_sql(question_01)
+        execute_sql_query(question_01, sql_query)
     elif button_example2:
-        sql_query = generate_sql("Which employee has the most evening shifts between 5th Feb 2023 and 15th Feb 2023")
-        execute_sql_query(sql_query)
+        sql_query = generate_sql(question_02)
+        execute_sql_query(question_02, sql_query)
     elif button_example3:
-        sql_query = generate_sql("List all morning shifts which belong to employees whose names are Andrew Krotz and Caroline Laurin")
-        execute_sql_query(sql_query)
+        sql_query = generate_sql(question_03)
+        execute_sql_query(question_03, sql_query)
     elif button_example4:
-        sql_query = generate_sql("Which hospital has the most shifts between 5th Feb 2023 and 10th Feb 2023")
-        execute_sql_query(sql_query)
+        sql_query = generate_sql(question_04)
+        execute_sql_query(question_04, sql_query)
     else:
         st.write("")
